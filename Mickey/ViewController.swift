dc94e28341a9c3e535301980c7f265d36a4c35f3
Lennart Wisbar <lewisbar.git@gmail.com>
2018-03-26 01:03:01 +0200

    Removed AVAudioSession code

--- Mickey/ViewController.swift
+++ Mickey/ViewController.swift
@@ -10,26 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var audio = Audio()
-
     @IBAction func listenButtonPressed(_ sender: UIButton) {
-        do {
-            try audio.record()
-        } catch {
-            alert(title: "Error", message: error.localizedDescription)
-        }
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        do {
-            try audio.prepareAudioSession()
-            try audio.prepareAudioRecorder(url: getDocumentsDirectory().appendingPathComponent("recording.m4a", isDirectory: false))
-            audio.prepareProximityMonitoring()
-        } catch {
-            alert(title: "Error", message: error.localizedDescription)
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +30,6 @@ class ViewController: UIViewController {
         let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
         alertController.addAction(defaultAction)
         self.present(alertController, animated: true, completion: nil)
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsDirectory = paths[0]
-        return documentsDirectory
     }
 }
 
