318683e4ce99de993ad5784c8f82bf92ae18e5dc
Lennart Wisbar <lewisbar.git@gmail.com>
2018-03-26 01:01:06 +0200

    Started AVCapureSession experiment

--- Mickey/ViewController.swift
+++ Mickey/ViewController.swift
@@ -7,20 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBAction func listenButtonPressed(_ sender: UIButton) {
+    let captureSession = AVCaptureSession()
+    
+    private func prepareCaptureSession() {
+        guard let audioCaptureDevice = AVCaptureDevice.default(for: .audio) else {
+            alert(title: "No input source", message: "The input device could not be initiated.")
+            return
+        }
         
+        do {
+            let audioInput = try AVCaptureDeviceInput(device: audioCaptureDevice)
+            captureSession.addInput(audioInput)
+        } catch {
+            alert(title: "Error", message: error.localizedDescription)
+        }
+        
+        // Output
+        let audioOutput = AVCaptureAudioDataOutput()
+        captureSession.addOutput(audioOutput)
+    }
+    
+    @IBAction func listenButtonPressed(_ sender: UIButton) {
+        if !captureSession.isRunning {
+            captureSession.startRunning()
+        } else {
+            captureSession.stopRunning()
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        prepareCaptureSession()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Helpers
+    func alert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Close", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
 
